d85d2107db38c2616964fa781616704613da9bb9
fritz <fritz@waterinsight.nl>
2011-09-23 11:52:08 +0200

    Next step implement theme and cron

--- automizer.py
+++ automizer.py
@@ -2,33 +2,29 @@
 import os, sys, datetime, subprocess
 from fileinput import input
 from maps.models import Map
+import settings
 
-mapfile = '/home/fritz/WebDev/algae2011.map'
+mapfile = '/home/fritz/WebDev/algae2010.map'
 txt = '/home/fritz/WebDev/layers.txt'
-js = '/home/fritz/WebDev/datepick/js/enabledates.js'
 
 def get_date_mapfile(mapfile, txt):
     print "getting mapfile, writing dates to txtfile"
     readdates = """grep -E "NAME" """ + mapfile + """ | sed -e 's/[^0-9]//g' -e '/^$/ d' > """ + txt
     subprocess.call(readdates, shell=True)
 
-rtxt = open(txt)
-wjs = open(js, "w")
-maps = []
-
 def read_write_dates():
+    rtxt = open(txt)
     print "reading text file saving dates as datetime objects"
     read_it = rtxt.readlines()
     del read_it[0:1]
+    maps = []
+    map_ = Map.objects.filter(url__endswith="2010.map")[0]
     for lines in read_it:
         line = lines.strip()
-        year = int(line[0:4])
-        month = int(line[4:6])
-        day = int(line[6:8])
-        datum = datetime.date(year, month, day)
-        maps.append(datum)
-    for i in range(len(maps)):
-        
+        month = int(line[0:2])
+        day = int(line[2:4])
+        datum = datetime.date(2011, month, day)
+        map_.mapdate_set.get_or_create(date=datum)
 
 get_date_mapfile(mapfile, txt)
 read_write_dates()
